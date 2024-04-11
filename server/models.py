e1cbce85683412cd1efc8c0cb58953b1d31440c8
David Kerr <kerrky@gmail.com>
2024-04-11 17:43:42 -0400

    Added packages and files

--- server/models.py
+++ server/models.py
@@ -1,6 +1,15 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 from config import db
 
 # Models go here!
+class Artist(db.Model, SerializerMixin):
+  __tablename__ = 'artists'
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, nullable=False, unique=True)
+  image = db.Column(db.String, nullable=False)
+
+# Finish models and then migrate then upgrade then add relationships
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,13 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, Artist
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
+        Artist.query.delete()
+
         print("Starting seed...")
         # Seed code goes here!
+        # artist1 = Artist(name='Korn', image='image.jpg')
